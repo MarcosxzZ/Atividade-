28a31120bf54fea99cdf5dfa4b072c51f8bd587a
MarcosxzZ <marcosviniciusamorimdasilva@gmail.com>
2022-10-11 21:44:22 -0300

    subida de arquivo js

---
+++ Exercio de sala de aula/JS 04 - Estruturas condicionais/estruturasDecisao.js
@@ -0,0 +1,16 @@
+var num1 = 10;
+var num2 = 50;
+var num3 = 3;
+
+if(num1>num2){
+if(num1>num3){
+console.log("Num1 é o menor!");
+}else
+console.log("Num3 é o menor!");
+}else if(num2>num1){
+if(num1>num3){
+console.log("Num2 é o menor!");
+}else
+console.log("Num3 é o menor!");
+}
+
