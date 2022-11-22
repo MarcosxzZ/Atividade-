db7323d48b55c940bf7b2d307c54d03def9495a3
MarcosxzZ <marcosviniciusamorimdasilva@gmail.com>
2022-11-21 21:56:28 -0300

    subida de aruivo JAVA

---
+++ JAVA/PrimeiroProjetoJava/src/calcular.java
@@ -0,0 +1,12 @@
+public class calcular {
+    //primeiro passo cria metodo main
+    public static void main (String[] args){
+        //Vamos somar a idade de três alunos
+        // var idade1 = 25
+       int idade1 = 25;
+       int idade2 = 15;
+       int somaIdade = idade1 + idade2;
+
+        System.out.println("A soma da idade dos alunos é:"+ somaIdade);
+    }
+}
---
+++ JAVA/PrimeiroProjetoJava/src/pipoca.java
@@ -0,0 +1,10 @@
+public class pipoca {
+    //mostrar no console a frase hello world
+    public static void main(String[]args){
+
+    String texto ="Hello World";
+    System.out.println(texto);
+    System.out.print(45);
+    //console.log("hello world")
+    }
+}
